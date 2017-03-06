e03eb199b6676b061b4b77df3ebd6230b92db1f3
SpaceFox <ujueseo.yeou@gmail.com>
2017-03-06 21:08:55 +0100

    TMP2

--- src/main/kotlin/fr/spacefox/kiops/Kiops.kt
+++ src/main/kotlin/fr/spacefox/kiops/Kiops.kt
@@ -7,6 +7,10 @@ import java.util.*
 
 const val SECTOR_SIZE: Int = 4096
 
+object BlockData {
+    var data: ByteArray = ByteArray(0)
+}
+
 fun main(args: Array<String>) {
     try {
         val kiops = Kiops(args[0])
@@ -58,8 +62,10 @@ class Kiops(private val path: String) {
     fun test() {
         val iops = (nbThreads + 1).toLong()  // Initial loop
         var blockSize: Long = 512
+        val threads: MutableList<KiopsThread> = mutableListOf()
         while (iops > Math.max(1, nbThreads / 4) && blockSize < mediaSize) {
-            val threads: MutableList<KiopsThread> = mutableListOf()
+            threads.clear()
+            BlockData.data = ByteArray(blockSize.toInt())
             for (i in 1..nbThreads) {
                 threads.add(KiopsThread(blockSize))
             }
@@ -95,14 +101,13 @@ class Kiops(private val path: String) {
             try {
                 RandomAccessFile(path, "r").use { raf ->
                     var position: Long
-                    val blockData = ByteArray(blockSize.toInt())
                     val endTime = System.currentTimeMillis() + 2000
                     while (System.currentTimeMillis() < endTime) {
                         count++
                         position = Math.abs(random.nextLong() % (mediaSize - blockSize))
                         position = position and (SECTOR_SIZE - 1).inv().toLong()
                         raf.seek(position)
-                        raf.read(blockData)
+                        raf.read(BlockData.data)
                     }
                 }
             } catch (e: FileNotFoundException) {
